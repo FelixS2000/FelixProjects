d7fc7d5ef92b8bdbfe7a7d75555de7c3830db857
Felix <fguzmanceri@gmail.com>
2023-10-10 18:25:12 -0400

    saving updates

--- CurrencyFlipper/scripts/script.js
+++ CurrencyFlipper/scripts/script.js
@@ -1,23 +1,31 @@
 async function getCurrencyOptions() {
-    const apiKey = '4991dffc0ccbf32dff71f879';
-    const response = await fetch(`https://v6.exchangeratesapi.io/latest?base=USD&apikey=${apiKey}`);
-    const data = await response.json();
-    const currencyOptions = Object.keys(data.rates);
+    const apiKey = 'LwtMFHe0d5Os0e5Z4RRZNyEek6jBu4cL';
+    try {
+        const response = await fetch(`https://api.apilayer.com/exchangerates_data/latest`, {
+            headers: {
+                'apikey': apiKey
+            }
+        });
+        const data = await response.json();
+        const currencyOptions = Object.keys(data.rates);
 
-    const fromCurrencySelect = document.getElementById('fromCurrency');
-    const toCurrencySelect = document.getElementById('toCurrency');
+        const fromCurrencySelect = document.getElementById('fromCurrency');
+        const toCurrencySelect = document.getElementById('toCurrency');
 
-    currencyOptions.forEach(currency => {
-        const option = document.createElement('option');
-        option.value = currency;
-        option.text = currency;
-        fromCurrencySelect.appendChild(option);
+        currencyOptions.forEach(currency => {
+            const option = document.createElement('option');
+            option.value = currency;
+            option.text = currency;
+            fromCurrencySelect.appendChild(option);
 
-        const option2 = document.createElement('option');
-        option2.value = currency;
-        option2.text = currency;
-        toCurrencySelect.appendChild(option2);
-    });
+            const option2 = document.createElement('option');
+            option2.value = currency;
+            option2.text = currency;
+            toCurrencySelect.appendChild(option2);
+        });
+    } catch (error) {
+        console.error('Error fetching currency options:', error);
+    }
 }
 
 async function convertCurrency() {
@@ -25,16 +33,24 @@ async function convertCurrency() {
     const fromCurrency = document.getElementById("fromCurrency").value;
     const toCurrency = document.getElementById("toCurrency").value;
 
-    const apiKey = 'YOUR_API_KEY';
-    const response = await fetch(`https://v6.exchangeratesapi.io/latest?base=${fromCurrency}&apikey=${apiKey}`);
-    const data = await response.json();
+    const apiKey = 'LwtMFHe0d5Os0e5Z4RRZNyEek6jBu4cL';
+    try {
+        const response = await fetch(`https://api.apilayer.com/exchangerates_data/latest`, {
+            headers: {
+                'apikey': apiKey
+            }
+        });
+        const data = await response.json();
 
-    if (data.error) {
-        document.getElementById("result").innerHTML = "Error: " + data.error.info;
-    } else {
-        const exchangeRate = data.rates[toCurrency];
-        const convertedAmount = (amount * exchangeRate).toFixed(2);
-        document.getElementById("result").innerHTML = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        if (data.error) {
+            document.getElementById("result").innerHTML = "Error: " + data.error.info;
+        } else {
+            const exchangeRate = data.rates[toCurrency];
+            const convertedAmount = (amount * exchangeRate).toFixed(2);
+            document.getElementById("result").innerHTML = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
+        }
+    } catch (error) {
+        console.error('Error converting currency:', error);
     }
 }
 
