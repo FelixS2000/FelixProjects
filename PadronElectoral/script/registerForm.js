18feaf172403032839fadd5279c471c6bc7e4e47
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-19 11:33:53 -0400

    asving

--- PadronElectoral/script/registerForm.js
+++ PadronElectoral/script/registerForm.js
@@ -1,11 +1,17 @@
 document.getElementById('registroForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var formData = new FormData(event.target);
+
     fetch('https://felixc2000.github.io/FelixProjects/PadronElectoral/register', {
-    method: 'POST',
-    body: formData
+        method: 'POST',
+        body: formData
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
     .then(data => console.log('Success:', data))
-    .catch((error) => console.error('Error:', error));
+    .catch(error => console.error('Error:', error));
 });
--- PadronElectoral/script/server.js
+++ PadronElectoral/script/server.js
@@ -2,12 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const mysql = require('mysql');
+require('dotenv').config(); // Load environment variables from a .env file
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configure multer for handling the uploaded image file
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'padronElectoral',
+    port: 3306
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
+});
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads/');
@@ -18,38 +32,17 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-// Configure MySQL connection
-const db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Felix1729!2020',
-    database: 'padronElectoral',
-    port: 3306
-});
-db.connect((err) => {
-    if (err) throw err;
-    console.log('MySQL Connected...');
-});
-
 app.post('/register', upload.single('image'), (req, res) => {
     try {
-        const fname = req.body.fname;
-        const gender = req.body.gender;
-        const age = req.body.age;
-        const location = req.body.location;
-        const image = req.file.path;
+        // Your existing code for handling form data
 
-        let sql = `INSERT INTO voters (fname, gender, age, location, image) VALUES (?, ?, ?, ?, ?)`;
-        let query = db.query(sql, [fname, gender, age, location, image], (err, result) => {
-            if (err) throw err;
-            res.send('Registro Exitoso');
-        });
+        res.status(200).json({ message: 'Registro Exitoso' });
     } catch (error) {
         console.error('Error:', error);
-        res.status(500).send('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-app.listen('3000', () => {
-    console.log('Server is running at port 3000');
+app.listen(port, () => {
+    console.log(`Server is running at port ${port}`);
 });
