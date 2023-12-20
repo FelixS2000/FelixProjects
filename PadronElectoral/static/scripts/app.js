1b08e7319e02d9cc5bbb2264f3830027b8aa39d6
Felix Guzman <fguzmanceri@gmail.com>
2023-12-20 11:43:09 -0400

    saving update

--- PadronElectoral/static/scripts/app.js
+++ PadronElectoral/static/scripts/app.js
@@ -1,22 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const voterList = document.getElementById('voter-list');
+const mysql = require('mysql');
 
-    // Fetch voters from the API
-    fetch('/api/voters')
-        .then(response => response.json())
-        .then(data => {
-            // Render voter list
-            data.forEach(voter => {
-                const voterCard = document.createElement('div');
-                voterCard.className = 'voter-card';
-                voterCard.innerHTML = `
-                    <h3>${voter.fname}</h3>
-                    <p>Gender: ${voter.gender}</p>
-                    <p>Age: ${voter.age}</p>
-                    <p>Location: ${voter.location}</p>
-                `;
-                voterList.appendChild(voterCard);
-            });
-        })
-        .catch(error => console.error('Error fetching voters:', error));
+const db = mysql.createConnection({
+    host: 'localhost', // MySQL server host address
+    user: 'root', // MySQL server username
+    password: 'your_password', // MySQL server password
+    database: 'your_database' // Your database name
+});
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('MySQL Connected...');
 });
