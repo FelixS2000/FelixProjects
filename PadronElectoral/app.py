dee37b33f65f47dcec2aab5262f3aa4291c5a428
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-20 10:16:34 -0400

    saving updates for MySQL

--- PadronElectoral/app.py
+++ PadronElectoral/app.py
@@ -1,29 +1,45 @@
 from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/padronElectoral'
-db = SQLAlchemy(app)
 
-class Voter(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fname = db.Column(db.String(255))
-    gender = db.Column(db.Enum('m', 'f'))
-    age = db.Column(db.Integer)
-    location = db.Column(db.String(255))
-    image = db.Column(db.LargeBinary)
+# MySQL Connection Configuration
+db_config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': 'Felix1729!2020',
+    'database': 'padronelectoral',
+    'port': 3306,
+    'raise_on_warnings': True
+}
 
-# Replace 'username' and 'password' with your MySQL username and password
+def execute_query(query, params=None):
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+        if params:
+            cursor.execute(query, params)
+        else:
+            cursor.execute(query)
+        connection.commit()
+        return cursor.fetchall()
+    except Error as e:
+        print(e)
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/api/voters')
+@app.route('/voters', methods=['GET'])
 def get_voters():
-    voters = Voter.query.all()
-    return jsonify([{'id': voter.id, 'fname': voter.fname, 'gender': voter.gender, 'age': voter.age, 'location': voter.location} for voter in voters])
+    query = "SELECT * FROM voters"
+    result = execute_query(query)
+    return jsonify(result)
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
