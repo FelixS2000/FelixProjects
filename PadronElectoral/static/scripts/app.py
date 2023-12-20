a811f2da94d8f1efbea28c01ed9b22230beb6b5f
Felix Guzman <fguzmanceri@gmail.com>
2023-12-20 13:42:30 -0400

    update app

--- PadronElectoral/static/scripts/app.py
+++ PadronElectoral/static/scripts/app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask, render_template
 import mysql.connector
 
@@ -11,37 +12,7 @@ db_config = {
     'database': 'padronElectoral',
 }
 
-# Create a MySQL connection
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
-
-# Create the voters table if not exists
-create_table_query = """
-CREATE TABLE IF NOT EXISTS voters (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  fname VARCHAR(255),
-  gender ENUM('m', 'f'),
-  age INT,
-  location VARCHAR(255),
-  image BLOB
-)
-"""
-cursor.execute(create_table_query)
-conn.commit()
-
-# Read and execute the padronelectoral.sql file
-with open('padronelectoral.sql', 'r') as sql_file:
-    sql_script = sql_file.read()
-    cursor.execute(sql_script)
-    conn.commit()
-
-# Route to display the list of voters
-@app.route('/')
-def index():
-    cursor.execute("SELECT * FROM voters")
-    voters = cursor.fetchall()
-    
-    return render_template('index.html', voters=voters)
+# ... (rest of your Flask application code)
 
 # Run the Flask application
 if __name__ == '__main__':
