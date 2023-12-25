cbd3a362fe6b5ac4e6e99267520c04fd51e01399
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 12:22:55 -0400

    saving updates

--- PadronElectoral/app.py
+++ PadronElectoral/app.py
@@ -1,43 +1,46 @@
 # app.py
 
-import sqlite3
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
-@app.route('/register', methods=['POST'])
-def register_voter():
-  connection = sqlite3.connect('database.db')
-  cursor = connection.cursor()
+# Configure db connection
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="password",
+  database="electoral"
+)
 
-  name = request.json['name']
-  age = request.json['age']
-  address = request.json['address']
-  photo = request.json['photo']
+cursor = db.cursor()
 
-  cursor.execute('INSERT INTO voters (name, age, address, photo) VALUES (?, ?, ?, ?)', (name, age, address, photo))
-
-  voter_id = cursor.lastrowid
-  voter = cursor.execute('SELECT * FROM voters WHERE id = ?', (voter_id,)).fetchone()
-
-  connection.commit()
-  connection.close()
-
-  return jsonify({
-  'id': voter_id,
-  'name': name, 
-  'age': age,
-  'address': address,
-  'photo': photo
-})
-
-def send_file():
-  pass
-
-@app.route('/') 
+@app.route('/')
 def index():
-  return send_file('index.html')
+  cursor.execute("SELECT * FROM voters")
+  voters = cursor.fetchall()  
+  return render_template('index.html', voters=voters)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if request.method == 'POST':
+    name = request.form['name']
+    age = request.form['age']
+    address = request.form['address']
+    photo = request.files['photo']
+    
+    # Save photo
+    photo.save(os.path.join('static', photo.filename))
+    
+    # Insert into db
+    sql = "INSERT INTO voters (name, age, address, photo) VALUES (%s, %s, %s, %s)"
+    cursor.execute(sql, (name, age, address, photo.filename))
+    db.commit()
+    
+    return redirect(url_for('index'))
+
+  return render_template('register.html')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
