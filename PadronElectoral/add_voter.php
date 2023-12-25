86ac95e833e9788380b62cf85224274a0dd8ef27
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 15:17:37 -0400

    saving

--- PadronElectoral/add_voter.php
+++ PadronElectoral/add_voter.php
@@ -1,33 +1,50 @@
 <?php
 
-// Connect to the database 
+// Connect to DB
 $dbhost = "127.0.0.1";
-$dbuser = "root";
+$dbuser = "root"; 
 $dbpass = "Felix1729!2020";
 $dbname = "electoral";
 
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
-$name = $_POST['name'];
-$age = $_POST['age']; 
-$address = $_POST['address'];
-$photo = addslashes(file_get_contents($_FILES['photo']['tmp_name']));
+// Check for form submission
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-// Insert voter query
-$stmt = $conn->prepare("INSERT INTO voters (name, age, address, photo) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("sisi", $name, $age, $address, $photo);
+  // Get form data
+  $name = $_POST['name'];
+  $age = $_POST['age'];
+  $address = $_POST['address'];
+  
+  // Upload photo
+  $photo = $_FILES['photo']['name'];
+  move_uploaded_file($_FILES['photo']['tmp_name'], "uploads/$photo");
 
-if ($stmt->execute()) {
-  echo "Voter added successfully"; 
-} else {
-  echo "Error adding voter: " . $conn->error;
+  // Insert query
+  $stmt = $conn->prepare("INSERT INTO voters (name, age, address, photo) VALUES (?, ?, ?, ?)");
+  $stmt->bind_param("sisi", $name, $age, $address, $photo);
+  
+  if($stmt->execute()) {
+    echo "Voter added";
+  }
+
 }
 
+// Select and display voters
+$sql = "SELECT id, name, age, address FROM voters";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) {
+  // Print table rows
+  echo $row['id'] . " " . $row['name']; 
+}
+
+// Close connection
 $stmt->close();
 $conn->close();
+
+?>
