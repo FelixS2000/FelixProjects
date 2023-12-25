4a37570bff47f2990a26b24763a94b9146ec6269
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 15:06:34 -0400

    saving updates

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,3 +1,6 @@
+// Import settings
+import settings from './settings.json' 
+
 // Get form element
 const form = document.getElementById('voterForm');
 
@@ -17,11 +20,20 @@ form.addEventListener('submit', e => {
   })
   .then(response => response.text())
   .then(data => {
+    
     // Redirect to register on success
     window.location.href = 'register.php';
+
   })
   .catch(error => {
     console.error(error);
   });
 
 });
+
+// Database settings from settings.json
+const db = settings.db;
+const dbHost = db.host;
+const dbUser = db.user;
+const dbPass = db.password;
+const dbName = db.name;
