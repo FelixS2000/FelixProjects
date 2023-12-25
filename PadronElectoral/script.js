76173ac5a97433abc69f762393981d281d2c51da
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 14:50:59 -0400

    saving

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,30 +1,45 @@
 // Get form element
 const form = document.getElementById('voterForm');
 
-// Add submit event listener 
-form.addEventListener('submit', (e) => {
 
-  // Prevent default form submission
+
+
+// Get form element
+const form = document.getElementById('voterForm');
+
+// Get name input element 
+const nameInput = form.elements.name;
+const ageInput = form.elements.age;
+const addressInput = form.elements.address;
+const photoInput = form.elements.photo;
+
+// Add submit event listener
+form.addEventListener("submit", (e) => {
+  // Prevent default submission
   e.preventDefault();
 
   // Get form data
   const formData = new FormData(form);
-  const name = formData.get('name');
-  const age = formData.get('age');
-  const address = formData.get('address');
 
-  // Send POST request to add_voter.php
-  fetch('add_voter.php', {
-    method: 'POST', 
-    body: formData
-  })
-  .then(response => response.text())
-  .then(data => {
-    // Display response message
-    alert(data);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+  // Get name value
+  const name = nameInput.value;
+  const age = ageInput.value;
+  const address = addressInput.value;
+  const photo = photoInput.value;
 
+  // Send post to the database
+  fetch("register.php", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.json())
+    .catch((error) => console.log("Error:", error))
+    .then((result) => {
+      if (!result) {
+        alert("Registration Failed");
+      } else {
+        alert(`Welcome ${result}`);
+        window.location.href = "index.html";
+      }
+    });
 });
