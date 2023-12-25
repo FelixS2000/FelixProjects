ddcc8a6869e8cb21815305634bcecedd39c2155e
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 14:13:18 -0400

    saving

--- PadronElectoral/add_voter.php
+++ PadronElectoral/add_voter.php
@@ -1,33 +1,50 @@
 <?php
 
-$host = "localhost"; 
-$user = "root";
-$password = "Felix1729!2020";
-$dbname = "electoral";
+// DB connection function
+function dbConnect() {
 
-// Load settings 
-$settings = json_decode(file_get_contents('settings.json'), true);
+  // Load settings
+  $settings = json_decode(file_get_contents('settings.json'), true);
 
-$conn = new mysqli($host, $user, $password, $dbname);
+  // Connect to DB
+  return new mysqli($settings['db']['127.0.0.1'], $settings['db']['root'], $settings['db']['Felix1729!2020'], $settings['db']['electoral']);
 
-$name = $_POST['name'];
-$age = $_POST['age'];
-$address = $_POST['address'];
-$photo = addslashes(file_get_contents($_FILES['photo']['tmp_name']));
-
-$sql = "INSERT INTO voters (name, age, address, photo) VALUES ('$name', '$age', '$address', '$photo')";
-
-if ($conn->query($sql) === TRUE) {
-  header("Location: add_voter.php");
-  exit();
-} else {
-  echo "Error adding voter: " . $conn->error;
-}
-if ($conn->query($sql) === FALSE) {
-  echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
+// Handle form submission 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-$conn->close();
+  // Connect to DB
+  $conn = dbConnect();
 
-?>
+  // Check required fields
+  if (empty($_POST['name']) || empty($_POST['age']) || empty($_POST['address']) || empty($_FILES['photo'])) {
+    // Redirect back to form
+    header("Location: index.html");
+    exit;
+  }
+
+  // Validate age is number
+  if (!is_numeric($_POST['age'])) { 
+    // Show error
+  }
+
+  // Prepare statement
+  $stmt = $conn->prepare("INSERT INTO voters (name, age, address, photo) VALUES (?, ?, ?, ?)");
+  
+  // Bind params
+  $stmt->bind_param('siss', $_POST['name'], $_POST['age'], $_POST['address'], $_POST['photo']); 
+
+  // Execute statement
+  if (!$stmt->execute()) {
+    // Show error
+  } else {
+    // Success, redirect
+    header("Location: register.php");
+    exit;
+  }
+
+  $stmt->close();
+  $conn->close();
+
+}
