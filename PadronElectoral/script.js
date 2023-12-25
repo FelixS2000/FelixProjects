426bfbef0747e5b143c6e99202cca43b732fe087
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 14:39:16 -0400

    ss

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,57 +1,30 @@
-// Get form elements
-const form = document.getElementById("voterForm");
+// Get form element
+const form = document.getElementById('voterForm');
 
-// Submit event handler
+// Add submit event listener 
 form.addEventListener('submit', (e) => {
 
+  // Prevent default form submission
   e.preventDefault();
-  
-  
 
-  // Create form data
-  const formData = new FormData();
+  // Get form data
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const age = formData.get('age');
+  const address = formData.get('address');
 
-  // Append form fields
-  formData.append('name', nameInput.value); 
-  formData.append('age', ageInput.value);
-  formData.append('address', addressInput.value);
-  formData.append('photo', photoInput.value);
-
-
-
-
-  // XMLHttpRequest
-  const xhr = new XMLHttpRequest();
-
-  // Open request
-  xhr.open('POST', 'add_voter.php');
-  // Send data on completion
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      alert(`Votre vote a bien été enregistré !`);
-      window.location.href='index.html';
-      } else {
-        console.error(`Error: ${xhr.status}`);
-        }
-        };
-
-  xhr.onerror = () => {
-    // Error
-    console.error('An error occurred while submitting the form!');
-  };
-
-  // Handle response 
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4) {
-      if(xhr.status === 200) {
-        // Success
-      } else {
-        // Handle error
-      }
-    }
-  };
-
-  // Send request 
-xhr.send(formData);
+  // Send POST request to add_voter.php
+  fetch('add_voter.php', {
+    method: 'POST', 
+    body: formData
+  })
+  .then(response => response.text())
+  .then(data => {
+    // Display response message
+    alert(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 });
