fa26bd2bef1560070a3a2ccc388a1f3b7c5c0618
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 23:39:32 -0400

    saving updates

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,36 +1,46 @@
-// script.js
+// Select elements 
+const form = document.getElementById('voterForm');
+const resultDiv = document.getElementById('result');
 
-// Define display result function
+// Form submit handler
+form.addEventListener('submit', async (e) => {
+
+  e.preventDefault();
+
+  // Get form data
+  const formData = new FormData(form);
+
+  // Submit form data
+  const response = await fetch('/api/voters', {
+    method: 'POST',
+    body: formData
+  });
+
+  // Handle response
+  if(response.ok) {
+    const data = await response.json();
+    displayResult(data);
+  } else {
+    console.error('Error submitting form');
+  }
+
+});
+
+// Display result 
 function displayResult(data) {
 
-  // Get result div
-  const resultDiv = document.getElementById('result');
-  
-  // Display data
   resultDiv.innerHTML = `
-    <h2>Submitted Data:</h2>
+    <h3>Submitted Data:</h3>
     <p>
       Name: ${data.name}<br>
       Age: ${data.age}<br>
-      Gender: ${data.gender}<br>
+      Gender: ${data.gender}<br>  
       Address: ${data.address}
     </p>
   `;
 
-  // Display image
   const img = document.createElement('img');
   img.src = data.photo;
   resultDiv.appendChild(img);
 
 }
-
-// Form submit handler
-form.addEventListener('submit', async (e) => {
-
-  // Submit logic...
-
-  const data = await response.json();
-
-  displayResult(data);
-
-});
