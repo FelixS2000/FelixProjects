11c9270c8c1fd77db7703facb90f685277a59908
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-25 23:30:13 -0400

    saving

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,49 +1,13 @@
-// Select elements
-const form = document.getElementById('voterForm');
-const errorDiv = document.getElementById('error');
+// script.js
 
-// Add submitted data div
-const resultsDiv = document.createElement('div');
-resultsDiv.id = 'results';
-document.body.appendChild(resultsDiv);
+// Define display result function
+function displayResult(data) {
 
-
-// Form submit handler
-form.addEventListener('submit', async (e) => {
-
-  // Get form data
-  const formData = new FormData(form);
-
-  // Get gender value
-  const gender = formData.get('gender');
-
-  // Append gender to form data
-  formData.append('gender', gender);
-
-  // POST request to API endpoint
-  fetch('/api/voters', {
-    method: 'POST', 
-    body: formData
-  })
-  .then(response => {
-    if(response.ok) {
-      return response.json();
-    }
-    throw new Error('Network response was not ok.');
-  })
-  .then(data => {
-    console.log('Success:', data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    errorDiv.textContent = 'There was a problem saving the data.';
-  });
-});
-
-
-async function displayResult(data) {
-
-  resultsDiv.innerHTML = `
+  // Get result div
+  const resultDiv = document.getElementById('result');
+  
+  // Display data
+  resultDiv.innerHTML = `
     <h2>Submitted Data:</h2>
     <p>
       Name: ${data.name}<br>
@@ -56,6 +20,17 @@ async function displayResult(data) {
   // Display image
   const img = document.createElement('img');
   img.src = data.photo;
-  resultsDiv.appendChild(img);
-  displayResult(data);
+  resultDiv.appendChild(img);
+
 }
+
+// Form submit handler
+form.addEventListener('submit', async (e) => {
+
+  // Submit logic...
+
+  const data = await response.json();
+
+  displayResult(data);
+
+});
