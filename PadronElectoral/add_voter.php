abb917e167efa67e2f73ac4c4631f8470d5de7cf
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-27 12:20:17 -0400

    saving

--- PadronElectoral/add_voter.php
+++ PadronElectoral/add_voter.php
@@ -1,15 +1,22 @@
 // add_voter.php
 
-// Get DB settings from settings.json
-header("Access-Control-Allow-Methods: POST"); 
 $settings = json_decode(file_get_contents('settings.json'), true);
-$db = $settings['db'];
+if (json_last_error() !== JSON_ERROR_NONE) {
+    die('Error reading settings.json');
+}
 
-// Connect to DB 
+
+// Connect to DB
 $conn = new mysqli($db['127.0.0.1'], $db['root'], $db['Felix1729!2020'], $db['electoral']);
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
 
-// Insert voter 
+// Insert voter
 $stmt = $conn->prepare("INSERT INTO voters (name, age, gender, address, photo) VALUES (?, ?, ?, ?, ?)");
+if (!$stmt) {
+    die('Query preparation failed: ' . $conn->error);
+}
 
 $stmt->bind_param('sisbs', $_POST['name'], $_POST['age'], $_POST['gender'], $_POST['address'], $_POST['photo']);
 
