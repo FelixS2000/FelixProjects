0df4c394ea21054877a3756c0f202ec938141956
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-27 11:08:26 -0400

    saving

--- PadronElectoral/add_voter.php
+++ PadronElectoral/add_voter.php
@@ -5,7 +5,7 @@ $settings = json_decode(file_get_contents('settings.json'), true);
 $db = $settings['db'];
 
 // Connect to DB 
-$conn = new mysqli($db['host'], $db['user'], $db['password'], $db['name']);
+$conn = new mysqli($db['127.0.0.1'], $db['root'], $db['Felix1729!2020'], $db['electoral']);
 
 // Insert voter 
 $stmt = $conn->prepare("INSERT INTO voters (name, age, gender, address, photo) VALUES (?, ?, ?, ?, ?)");
