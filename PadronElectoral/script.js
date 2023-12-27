2bef7203615e623fddaace35ca56c8955f2b6140
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-27 13:35:18 -0400

    saving

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,41 +1,89 @@
-// Fetch settings
-fetch('https://felixc2000.github.io/FelixProjects/PadronElectoral/settings.json')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Failed to fetch settings');
+document.addEventListener('DOMContentLoaded', function () {
+  const voterForm = document.getElementById('voterForm');
+  const resultDiv = document.getElementById('result');
+  const errorFooter = document.getElementById('error');
+
+  voterForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    const formData = new FormData(voterForm);
+    const name = formData.get('name');
+    const age = formData.get('age');
+    const gender = formData.get('gender');
+    const address = formData.get('address');
+    const photo = formData.get('photo');
+
+    // Basic validation
+    let errors = [];
+    if (!name) errors.push('Name is required');
+    if (!age) errors.push('Age is required');
+    if (!gender) errors.push('Gender is required');
+    if (!address) errors.push('Address is required');
+
+    if (errors.length > 0) {
+      errorFooter.textContent = errors.join(', ');
+      return;
     }
-    return response.json();
-  })
-  .then(data => {
 
-    // Get add voter URL
-    const displayVoterUrl = data.displayVoterUrl;
+    // Display result
+    resultDiv.innerHTML = `
+      <h3>Voter Information</h3>
+      <img src="${URL.createObjectURL(photo)}">
+      <p>Name: ${name}</p>
+      <p>Age: ${age}</p>
+      <p>Gender: ${gender}</p>
+      <p>Address: ${address}</p>
+    `;
 
-    // Use URL for fetching voter data
-    if (displayVoterUrl) {
-      // Perform the GET request for voter data
-      return fetch(displayVoterUrl);
-    } else {
-      console.error('display_voter.php URL not defined in settings');
-      throw new Error('display_voter.php URL not defined in settings');
-    }
+    // Perform the POST request to add the voter
+    fetch('https://felixc2000.github.io/FelixProjects/PadronElectoral/add_voter.php', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to add voter');
+        }
+        return response.text();
+      })
+      .then(responseText => {
+        // Display success message or handle it as needed
+        console.log(responseText);
+      })
+      .catch(error => {
+        console.error('Error adding voter:', error.message || error);
+      });
+  });
 
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Failed to fetch voter data. Status: ${response.status}`);
-    }
-    return response.text();
-  })
-  .then(responseText => {
-    // Attempt to parse the response text as JSON
-    try {
-      const voters = JSON.parse(responseText);
+  // Fetch voter data from the specified URL in settings
+  fetch('https://felixc2000.github.io/FelixProjects/PadronElectoral/settings.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch settings');
+      }
+      return response.json();
+    })
+    .then(data => {
+      const displayVoterUrl = data.displayVoterUrl;
 
+      if (displayVoterUrl) {
+        // Perform the GET request for voter data
+        return fetch(displayVoterUrl);
+      } else {
+        console.error('display_voter.php URL not defined in settings');
+        throw new Error('display_voter.php URL not defined in settings');
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to fetch voter data. Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(voters => {
       // Display voter data in the result div
-      const resultDiv = document.getElementById('result');
       if (voters && voters.length > 0) {
-        resultDiv.innerHTML = '<h3>Voter Information</h3>';
+        resultDiv.innerHTML += '<h3>Voter Information</h3>';
         voters.forEach(voter => {
           resultDiv.innerHTML += `
             <img src="${voter.photo}">
@@ -46,55 +94,10 @@ fetch('https://felixc2000.github.io/FelixProjects/PadronElectoral/settings.json'
           `;
         });
       } else {
-        resultDiv.innerHTML = 'No voters found';
+        resultDiv.innerHTML += 'No voters found';
       }
-    } catch (error) {
-      console.error('Error parsing JSON:', error);
-      throw new Error('Error parsing JSON');
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching voter data:', error.message || error);
-    throw new Error('Error fetching voter data');
-  });
-  
-  
-
-
-
-const voterForm = document.getElementById('voterForm');
-const resultDiv = document.getElementById('result');
-const errorFooter = document.getElementById('error');
-
-voterForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent form submission
-
-  const formData = new FormData(voterForm);
-  const name = formData.get('name');
-  const age = formData.get('age');
-  const gender = formData.get('gender');
-  const address = formData.get('address');
-  const photo = formData.get('photo');
-
-  // Basic validation
-  let errors = [];
-  if (!name) errors.push('Name is required');
-  if (!age) errors.push('Age is required');
-  if (!gender) errors.push('Gender is required');
-  if (!address) errors.push('Address is required');
-
-  if (errors.length > 0) {
-    errorFooter.textContent = errors.join(', ');
-    return;
-  }
-
-  // Display result
-  resultDiv.innerHTML = `
-    <h3>Voter Information</h3>
-    <img src="${URL.createObjectURL(photo)}">
-    <p>Name: ${name}</p>
-    <p>Age: ${age}</p>
-    <p>Gender: ${gender}</p>
-    <p>Address: ${address}</p>
-  `;
+    })
+    .catch(error => {
+      console.error('Error fetching voter data:', error.message || error);
+    });
 });
