5cbd24cee32fc453528fdfdfaa38d610144c7d90
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-28 23:52:46 -0400

    saving updates

--- PadronElectoral/add_voter.php
+++ PadronElectoral/add_voter.php
@@ -1,34 +1,38 @@
 <?php
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = htmlspecialchars($_POST["name"]);
-    $age = htmlspecialchars($_POST["age"]);
-    $gender = htmlspecialchars($_POST["gender"]);
-    $address = htmlspecialchars($_POST["address"]);
-    // Note: You should handle file uploads securely, and this example is simplified for demonstration purposes.
-    // You may want to store uploaded files in a secure directory and validate file types, size, etc.
-    $photo = $_FILES["photo"]["name"];
-    
-    // Create an associative array for the voter data
-    $voterData = array(
-        "name" => $name,
-        "age" => $age,
-        "gender" => $gender,
-        "address" => $address,
-        "photo" => $photo
-    );
+// Retrieve the form data
+$name = $_POST['name'];
+$address = $_POST['address'];
+$gender = $_POST['gender'];
+$age = $_POST['age'];
+$picture = $_FILES['picture']['name'];
 
-    // Convert the array to JSON format
-    $jsonData = json_encode($voterData);
+// Move the uploaded picture to a designated folder
+$targetDir = "uploads/";
+$targetFile = $targetDir . basename($picture);
+move_uploaded_file($_FILES['picture']['tmp_name'], $targetFile);
 
-    // Save the voter data to a file (for demonstration purposes)
-    file_put_contents("voters.json", $jsonData);
+// Save the voter information to the database
+$dbHost = "127.0.0.1";
+$dbUser = "root";
+$dbPassword = "Felix1729!2020";
+$dbName = "electoral";
 
-    // Provide a success message
+// Connect to the database
+$conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare and execute the SQL query
+$sql = "INSERT INTO voters (name, address, gender, age, picture) VALUES ('$name', '$address', '$gender', '$age', '$picture')";
+if ($conn->query($sql) === TRUE) {
     echo "Voter added successfully!";
 } else {
-    // Handle cases where the form is not submitted properly
-    echo "Invalid request!";
+    echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+// Close the database connection
+$conn->close();
 ?>
--- PadronElectoral/display_voter.php
+++ PadronElectoral/display_voter.php
@@ -1,48 +1,35 @@
 <?php
+// Retrieve the voter information from the database
+$dbHost = "127.0.0.1";
+$dbUser = "root";
+$dbPassword = "Felix1729!2020";
+$dbName = "electoral";
 
-// Read database connection details from settings.json
-$settingsJson = file_get_contents("settings.json");
-$settings = json_decode($settingsJson, true);
+// Connect to the database
+$conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
 
-// Check if the file is not empty
-if (!empty($settings)) {
-    // Extract database connection details
-    $dbHost = $settings["db"]["127.0.0.1"];
-    $dbUser = $settings["db"]["root"];
-    $dbPassword = $settings["db"]["Felix1729!2020"];
-    $dbName = $settings["db"]["electoral"];
-
-    // Create a database connection
-    $conn = new mysqli($dbHost, $dbUser, $dbPassword, $dbName);
-
-    // Check the connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // Fetch voter data from the database
-    $sql = "SELECT * FROM voters";
-    $result = $conn->query($sql);
-
-    // Check if there are rows in the result
-    if ($result->num_rows > 0) {
-        // Fetch all rows and convert to an associative array
-        while ($row = $result->fetch_assoc()) {
-            $voters[] = $row;
-        }
-
-        // Output the voter data as JSON
-        header("Content-Type: application/json");
-        echo json_encode($voters);
-    } else {
-        // Handle cases where no voters are found
-        echo "No voters found!";
-    }
-
-    // Close the database connection
-    $conn->close();
-} else {
-    // Handle cases where the settings are not available
-    echo "Invalid settings!";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// Prepare and execute the SQL query
+$sql = "SELECT * FROM voters";
+$result = $conn->query($sql);
+
+// Display the voter information
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "Name: " . $row['name'] . "<br>";
+        echo "Address: " . $row['address'] . "<br>";
+        echo "Gender: " . $row['gender'] . "<br>";
+        echo "Age: " . $row['age'] . "<br>";
+        echo "Picture: <img src='uploads/" . $row['picture'] . "'><br><br>";
+    }
+} else {
+    echo "No voters found.";
+}
+
+// Close the database connection
+$conn->close();
 ?>
