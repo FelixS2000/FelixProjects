9430457c51d911b1cfdae849a9bdc85d05d97b80
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2023-12-30 16:52:32 -0400

    saving

--- PadronElectoral/script.js
+++ PadronElectoral/script.js
@@ -1,23 +1,23 @@
 // script.js
 document.getElementById('addVoterForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  fetch('add_voter.php', {
-      method: 'POST',
-      body: formData
-  })
-      .then(response => response.text())
-      .then(data => console.log(data))
-});
-
-fetch('display_voter.php')
-  .then(response => response.json())
-  .then(data => {
-      const votersList = document.getElementById('votersList');
-      data.forEach(voter => {
-          const listItem = document.createElement('p');
-          listItem.textContent = `${voter.name} - ${voter.age} años - ${voter.voter_id}`;
-          votersList.appendChild(listItem);
-      });
-  })
+    event.preventDefault();
+  
+    const formData = new FormData(event.target);
+    fetch('add_voter.php', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => response.text())
+        .then(data => console.log(data))
+  });
+  
+  fetch('display_voter.php')
+    .then(response => response.json())
+    .then(data => {
+        const votersList = document.getElementById('votersList');
+        data.forEach(voter => {
+            const listItem = document.createElement('p');
+            listItem.textContent = `${voter.name} - ${voter.age} años - ${voter.voter_id}`;
+            votersList.appendChild(listItem);
+        });
+    })
