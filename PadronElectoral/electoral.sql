f2ffc8a2576c5f230dd54b24a914ba9c5d142e7d
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2024-01-01 12:00:37 -0400

    sainvg

---
+++ PadronElectoral/electoral.sql
@@ -0,0 +1,11 @@
+-- electoral.sql
+CREATE DATABASE electoral;
+USE electoral;
+CREATE TABLE voters (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  gender VARCHAR(10),
+  photo BLOB
+);
