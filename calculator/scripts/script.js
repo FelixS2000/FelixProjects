0e0fb051bdb57ba5cd91d2be59172be62ea3a84e
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2024-03-29 00:09:32 +0100

    saving

--- calculator/scripts/script.js
+++ calculator/scripts/script.js
@@ -1,69 +1,27 @@
-//function for the calculator using id
-function add(n1, n2) {
-  let s1 = document.getElementById(n1);
-  let s2 = document.getElementById(n2);
-  let result = Number(s1.value) + Number(s2.value);
-  let displayResult = document.getElementById('result');
-  displayResult.innerHTML = "=";
-  displayResult.style.color = 'green';
-  setTimeout(() => {
-    displayResult.innerHTML = result;
-  }, 500);
+function add(){
+    let num1 = document.getElementById('num1');
+    let num2 = document.getElementById('num2');
+    let result = Number(num1) + Number(num2);
+    document.getElementById('result').innerHTML = result;
 }
 
-function substract(n1, n2) {
-  let r1 = document.getElementById(n1);
-  let r2 = document.getElementById(n2);
-  let result = Number(r1.value) - Number(r2.value);
-  let displayResult = document.getElementById('result');
-  displayResult.innerHTML = "=";
-  displayResult.style.color = 'green';
-  setTimeout(() => {
-    displayResult.innerHTML = result;
-  }, 500);
+function subtract(){
+  let num1 = document.getElementById('num1');
+  let num2 = document.getElementById('num2');
+  let result = Number(num1) - Number(num2);
+  document.getElementById('result').innerHTML = result;
 }
 
-function multiply(n1, n2) {
-    let m1 = document.getElementById(n1);
-    let m2 = document.getElementById(n2);
-    let result = Number(m1.value) * Number(m2.value);
-    let displayResult = document.getElementById('result');
-    displayResult.innerHTML = "=";
-    displayResult.style.color = 'green';
-    setTimeout(() => {
-      displayResult.innerHTML = "";
-      displayResult.removeAttribute("style");
-    }, 800);
+function multiply(){
+    let num1 = document.getElementById('num1');
+    let num2 = document.getElementById('num2');
+    let result = Number(num1) * Number(num2);
+    document.getElementById('result').innerHTML = result;
 }
 
-function divide(n1, n2) {
-    let d1 = document.getElementById(n1);
-    let d2 = document.getElementById(n2);
-    let result = Number(d1.value) / Number(d2.value);
-    let displayResult = document.getElementById('result');
-    displayResult.innerHTML = "=";
-    displayResult.style.color = 'green';
-    setTimeout(() => {
-      displayResult.innerHTML = "";
-      displayResult.removeAttribute("style");
-    }, 800);
+function divide(){
+    let num1 = document.getElementById('num1');
+    let num2 = document.getElementById('num2');
+    let result = Number(num1) / Number(num2);
+    document.getElementById('result').innerHTML = result;
 }
-//displaying the actual date
-
-function displayDate() {
-  let date = new Date();
-  let day = date.getDate();
-  let month = date.getMonth() + 1;
-  let year = date.getFullYear();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
-  let second = date.getSeconds();
-  let displayDate = document.getElementById('date');
-  displayDate.innerHTML = day + "/" + month + "/" + year + " " + hour + ":" + minute + ":" + second;
-  setTimeout(() => {
-    displayDate.innerHTML = "";
-    displayDate.removeAttribute("style");
-  }, 800);
-}
-setInterval(displayDate, 1000);
-
