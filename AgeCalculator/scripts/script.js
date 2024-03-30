bb6d223c235a9409e5be5d1ac075f35863151fa9
FelixC2000 <147532247+FelixC2000@users.noreply.github.com>
2024-03-30 22:03:25 +0100

    saving update

--- AgeCalculator/scripts/script.js
+++ AgeCalculator/scripts/script.js
@@ -2,5 +2,7 @@ function substractCalculate(){
     const year1 = parseFloat(document.getElementById("year").value);
     const year2 = parseFloat(document.getElementById("year2").value);
     const rest = Number(year1) - Number(year2);
-    document.getElementById("result").innerHTML = rest;
+    document.getElementById("result").innerHTML = "Your age is : "+rest;
+    const date = new Date(year1, 0 ,1).getTime();
+    document.getElementById("date").innerHTML = date;
 }
